fcd722bf8605562eb82df3b111f912dfa8b47338
Arkady Zelensky <arkasha9606@gmail.com>
2019-02-11 14:28:22 +0200

    Updates

---
+++ precache-manifest.23a17a86e19a10999949e3ba2769d942.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/nice-alert/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c951fb9f45019cd8f99e",
+    "url": "/nice-alert/static/js/runtime~main.c951fb9f.js"
+  },
+  {
+    "revision": "7756d35b843b607fdd57",
+    "url": "/nice-alert/static/js/main.7756d35b.chunk.js"
+  },
+  {
+    "revision": "d16a266844ec01aafc8a",
+    "url": "/nice-alert/static/js/2.d16a2668.chunk.js"
+  },
+  {
+    "revision": "7756d35b843b607fdd57",
+    "url": "/nice-alert/static/css/main.891411e9.chunk.css"
+  },
+  {
+    "revision": "31fe91e84b5767afaed428f8f5a72013",
+    "url": "/nice-alert/index.html"
+  }
+];
