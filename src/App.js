78a762f9fc8e82b3c01ca2a6556d43842cc723fe
Arkady Zelensky <arkasha9606@gmail.com>
2019-02-12 12:23:10 +0200

    animation start

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.scss';
+import str from './strings';
 
 class NiceAlert extends Component {
   render() {
     return (
       <div className='nice-alert'>
-
+        <div className='nice-alert-inner'>
+          <p>{this.props.text}</p>
+        </div>
       </div>
     );
   }
@@ -15,11 +17,37 @@ class NiceAlert extends Component {
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+
+    const url = new URL(window.location.href);
+    const id = url.searchParams.get('id');
+    const msg = url.searchParams.get('msg');
+
+    let text = '';
+    if(str[id]) {
+      text = str[id];
+    }
+    else if(msg) {
+      text = msg;
+    }
+
+    this.state = {
+      text,
+    }
+  }
+
+  componentDidMount() {
+
+    // this.setState({text});
+  }
+
   render() {
     return (
       <div className="App">
 
-        <NiceAlert/>
+        <NiceAlert text={this.state.text}/>
 
       </div>
     );
